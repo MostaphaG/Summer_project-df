9fb9a9f68b155d345490f998433b3f27cac13723
MACIEK1JAREMA <87180511+MACIEK1JAREMA@users.noreply.github.com>
2021-07-15 19:22:42 +0100

    My progress on 2-forms and update on doc.
    Includes the file I have been working on, to process 2 forms of R^m. The method is not complete and includes some problems, so I explained these in the TEX file.

---
+++ 2 forms on R^m.py
@@ -0,0 +1,181 @@
+# 2 forms attempt 3 - generalising the diagonal extraction to more then 2x2 matricies (R^m for m > 2)
+
+# import modules
+import timeit
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy import symbols, diff
+from sympy.parsing.sympy_parser import parse_expr
+
+# %%
+
+# start the timer
+start = timeit.default_timer()
+
+
+# define a function to replace input string to be 'python understood'
+def format_eq(string):
+    # replace all the x and y with xg and yg:
+    string = string.replace('x', 'xg')
+    string = string.replace('y', 'yg')
+    # where there are special functions, replace them with library directions
+    string = string.replace('pi', 'np.pi')
+    string = string.replace('sqrt', 'np.sqrt')
+    string = string.replace('sin', 'np.sin')
+    string = string.replace('cos', 'np.cos')
+    string = string.replace('tan', 'np.tan')
+    string = string.replace('^', '**')
+    string = string.replace('ln', 'np.log')        
+    return string
+
+
+# define a function that takes input string that is python understood and turn into vector components:
+def eq_to_comps(string_x, string_y, xg, yg, u, v):
+    global equation_x, equation_y
+    # use this fucntion to replace given string to python understood equations:
+    equation_x = format_eq(string_x)
+    equation_y = format_eq(string_y)
+    # use these to define the field:
+    # also: check if equation equals zero, to then replace it with an array and not just 0:
+    if equation_x == '0':
+        u = np.zeros(np.shape(xg))
+        v = eval(equation_y)
+    elif equation_y == '0':
+        u = eval(equation_x)
+        v = np.zeros(np.shape(yg))
+    elif equation_x and equation_y == '0':
+        u = np.zeros(np.shape(xg))
+        v = np.zeros(np.shape(yg))
+    else:
+        u = eval(equation_x)
+        v = eval(equation_y)
+    # return these
+    return u, v
+
+
+# define scale of the graph
+L = 5
+pt_den = 21   # number of points on each axis
+
+# define x and y values
+x = np.linspace(-L, L, pt_den)
+y = np.linspace(-L, L, pt_den)
+z = np.linspace(-L, L, pt_den)
+
+# create a grid on x-y plane
+xg, yg = np.meshgrid(x, y)
+zg = np.meshgrid(z)
+
+# define a scaling factor
+a = 0.05
+
+# define an example vector field, now - from string, even initially
+string_x = 'x*y*z'  # x component
+string_y = '4*z*x**2'  # y component
+string_z = 'z*y*sin(x)'  # z component
+
+
+# to define a 2 from, need to perform the exterior derrivative on
+# the given 1 form (vector field).
+# To do this, seed to define partial derrivatives.
+# to make it general, need to define it through derrivatives w.r.t. not
+# already present coordinates.
+
+# set the dimensionality
+m = 3
+
+# take the input strings and turn them into sympy expressions to be able to
+# use sympy's partial differentiation
+sympy_expr_x = parse_expr(string_x, evaluate=False)
+sympy_expr_y = parse_expr(string_y, evaluate=False)
+sympy_expr_z = parse_expr(string_z, evaluate=False)
+# for m > 2, need more components, and need these in 'expressions' too!
+
+# define a sympy expression for string 0
+sympy_expr_zero = parse_expr('0*x', evaluate=False)
+
+# combine the 2 intoa list:
+expressions = np.array([sympy_expr_x, sympy_expr_y, sympy_expr_z])
+
+# set up an array to store derrivatives.
+ext_ds = np.empty((m, m), dtype='object')
+
+# use sympy partial derrivatives on these, as to get a 2-form on R2:
+# need to differentiate each component w.r.t the coordinates that it's
+# elementary 1 form does not contain.
+
+# set up an array of coordinates that need to be used (in standard order)
+coords = ['x', 'y', 'z']
+
+# set up an array to store the results
+# in 2D only dx^dy, in 3D (in order): dx^dy, dz^dx, dy^dz
+result = np.empty((int((m-1)*m/2), 1), dtype='object')
+for i in range(int((m-1)*m/2)):
+    result[i] = str(result[i])
+
+# loop over differentiating each, when differentiating w.r.t its coord, set to 0
+for coord_index in range(len(coords)):
+    # loop over differentiating each component:
+    for comp_index in range(len(expressions)):
+        # when equal set to 0, when not-differentiate:
+        if comp_index == coord_index:
+            ext_ds[comp_index, coord_index] = str(sympy_expr_zero)
+        elif comp_index != coord_index:
+            ext_ds[comp_index, coord_index] = str(diff(expressions[comp_index], coords[coord_index]))
+        # change the signs for wedges in wrong order, this will include lower left side of the matrix ext_ds
+        if comp_index > coord_index:
+            ext_ds[comp_index, coord_index] = ' - (' + str(ext_ds[comp_index, coord_index]) + ')'
+        elif comp_index < coord_index:
+            ext_ds[comp_index, coord_index] = ' + ' + str(ext_ds[comp_index, coord_index])
+
+
+# merge the results into a 2 form (for 2-form on R^2, the result is a single component (dx^xy))
+# do so by adding the diagonal ( / ) components of ext_ds
+
+# loop to extract the values from each diagonal ( / )
+for d in range(1, 2*m - 2):
+    if d <= m-1:  
+        # extract all values from the diagonal ( / )
+        for i in range(d+1):
+            if i == 0:
+                result[d-1, 0] = ''
+            j = d-i
+            temp = ext_ds[i, j]
+            # to exclude the zeros from derivatives:
+            if (temp == '0') or (temp == '-(0)') or (temp == '0*x'):
+                pass
+            else:
+                result[d-1, 0] += temp
+    if d > m-1:
+        for j in range(1, d):
+            if j == 1:
+                result[d-1, 0] = ''
+            i = d-j
+            temp = ext_ds[i, j]
+            # to exclude the zeros from derivatives:
+            if (temp == '0') or (temp == '-(0)') or (temp == '0*x'):
+                pass
+            else:
+                result[d-1, 0] += temp
+    # format the result to be 'python understood' to be able to use the eval()
+    result[d-1, 0] = format_eq(result[d-1, 0])
+
+print('\n')
+print(' the resulting 2 form is :')
+print(result)
+print('\n')
+print('the matrix of derrivatives was:')
+print(ext_ds)
+print('\n')
+
+# set up a vector to store the 2 form numerically, from xg and yg
+form_2 = np.empty((m, pt_den, pt_den, pt_den))
+
+# convert the sympy expressions back into strings to evaluate:
+for d in range(0, 2*m-3):
+    form_2[d, :, :, :] = eval(result[d, 0])  # numerical evaluation of the 2 form on R^2, at all defined grid points in R^2
+
+# return time to run
+stop = timeit.default_timer()
+print('Time to run: ', stop - start)
+
