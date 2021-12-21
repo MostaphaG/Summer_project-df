98e0d449d567ce82bedd37cb9561ab0b034d897b
MACIEK1JAREMA <87180511+MACIEK1JAREMA@users.noreply.github.com>
2021-12-21 10:30:09 +0000

    Added a file where we can produce figures for the paper
    So far, Only put suggested comments down, probably too many examples there now. Need to discuss what examples we should include and which ones aren't necessary.

---
+++ Library/formpy paper figures.py
@@ -0,0 +1,42 @@
+import formpy as fp
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
+
+# Figure presenting funcitonality of a basic Vector field
+
+
+# %%
+
+# Figure for basic 0-form
+
+
+# %%
+
+# Figure for basic 1-form
+
+
+# %%
+
+# Figure for basic 2-form
+
+
+# %%
+
+# Figure to show Hodge operations 
+
+
+# %%
+
+# Figure to show exterior derivative 
+
+# %%
+
+# Figure to show wedge
+
+
+# %%
+
+# Figure to show the interior derivative (possibly with metirc inc.)
+
