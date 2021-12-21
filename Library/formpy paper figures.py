2b7cad52ff48837e556a82cc2c3f071d5ec7f723
MACIEK1JAREMA <87180511+MACIEK1JAREMA@users.noreply.github.com>
2021-12-21 10:45:16 +0000

    Started trying to produce figure, for Vector fields, why is zoom_inset commented out, did it not work fine?

--- Library/formpy paper figures.py
+++ Library/formpy paper figures.py
@@ -6,6 +6,28 @@ import matplotlib.pyplot as plt
 
 # Figure presenting funcitonality of a basic Vector field
 
+# set up the axis grids, figure and axis 
+r = np.linspace(-5, 5, 15)
+xg, yg = np.meshgrid(r, r)
+fig = plt.figure(figsize=(7, 7))
+ax = fig.gca()
+ax.set_aspect('equal')
+
+# set up the vecotr field e^x and e^y components
+u = xg*np.cos(yg)
+v = yg*np.sin(xg)
+
+# set up the vector filed object
+field = fp.vector_field(xg, yg, u, v, fig=fig)
+field.give_eqn('x*cos(y)','y*sin(x)')
+
+field.plot()
+
+# The method zoom_inset creates the zoomed field, creates the inset axis and plots the field on the inset axis.
+# Target, zoom and dpd carry over from original zoom.
+field.zoom_inset((3, -3), 10, 9)
+
+
 
 # %%
 
