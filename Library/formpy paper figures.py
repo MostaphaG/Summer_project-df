83cda7e75b50593cf3fd83d23330c61e794f3994
MACIEK1JAREMA <87180511+MACIEK1JAREMA@users.noreply.github.com>
2021-12-28 19:22:16 +0000

    VF example for paper
    Code for the first 'proposal' figure for the VF example on paper.

--- Library/formpy paper figures.py
+++ Library/formpy paper figures.py
@@ -9,6 +9,74 @@ import matplotlib.pyplot as plt
 
 # %%
 
+# Vector field with insets: (zoom, DF, div and curl), as subplots
+
+
+# Set up the Vector Field
+r = np.linspace(-5, 5, 31)
+xg, yg = np.meshgrid(r, r)
+
+u = yg*np.sin(xg)
+v = -xg*np.cos(yg)
+
+VF = fp.vector_field(xg, yg, u, v)
+VF.give_eqn('y*sin(x)','-x*cos(y)')
+
+# set up subplots for different zooming in values
+fig = plt.figure(figsize=(10, 10))
+ax1 = fig.add_subplot(221)
+ax2 = fig.add_subplot(222)
+ax3 = fig.add_subplot(223)
+ax4 = fig.add_subplot(224)
+
+ax1.set_aspect('equal')
+ax1.set_xlabel(r'$x$')
+ax1.set_ylabel(r'$y$')
+ax1.set_title(r'$VF \ with \ Zoom \ Inset$')
+ax2.set_aspect('equal')
+ax2.set_xlabel(r'$x$')
+ax2.set_ylabel(r'$y$')
+ax2.set_title(r'$VF \ with \ DF \ Inset$')
+ax3.set_aspect('equal')
+ax3.set_xlabel(r'$x$')
+ax3.set_ylabel(r'$y$')
+ax3.set_title(r'$VF \ with \ Div \ Inset$')
+ax4.set_aspect('equal')
+ax4.set_xlabel(r'$x$')
+ax4.set_ylabel(r'$y$')
+ax4.set_title(r'$VF \ with \ Curl \ Inset$')
+
+# plot VF on all
+VF.plot(ax1)
+VF.plot(ax2)
+VF.plot(ax3)
+VF.plot(ax4)
+
+
+# zoom on each and plot
+
+# ax1
+zoomed_ax1, zoomed_VF = VF.zoom(target=[2, 3], zoom=2, dpd=7, inset=True, axis=ax1)
+zoomed_VF.colour('r')
+zoomed_VF.plot(zoomed_ax1)
+
+# ax2
+df_ax1, df_VF = VF.DF(target=[2, 3], zoom=2, dpd=7, inset=True, axis=ax2)
+df_VF.colour('r')
+df_VF.plot(df_ax1)
+
+# ax3
+div_ax1, div_VF = VF.Div(target=[2, 3], zoom=2, dpd=7, inset=True, axis=ax3)
+div_VF.colour('r')
+div_VF.plot(div_ax1)
+
+# ax4
+curl_ax1, curl_VF = VF.Curl(target=[2, 3], zoom=2, dpd=7, inset=True, axis=ax4)
+curl_VF.colour('r')
+curl_VF.plot(curl_ax1)
+
+# %%
+
 # 1-form plot and zooming on it
 
 r = np.linspace(-5, 5, 31)
@@ -48,12 +116,12 @@ zoomed_ax, zoomed_form = form1.zoom(target=[2, 3.5], zoom=1, dpd=7, inset=True, 
 zoomed_form.colour('r')
 zoomed_form.plot(zoomed_ax)
 
-# ax1
+# ax2
 zoomed_ax, zoomed_form = form1.zoom(target=[2, 3.5], zoom=10, dpd=7, inset=True, axis=ax2)
 zoomed_form.colour('r')
 zoomed_form.plot(zoomed_ax)
 
-# ax1
+# ax3
 zoomed_ax, zoomed_form = form1.zoom(target=[2, 3.5], zoom=100, dpd=7, inset=True, axis=ax3)
 zoomed_form.colour('r')
 zoomed_form.plot(zoomed_ax)
@@ -106,7 +174,7 @@ zoomed_form.plot(zoomed_ax)
 # %%
 
 '''
-Showcasing
+Showcasing, Perhaps most for an Appendix?
 '''
 
 # %%
