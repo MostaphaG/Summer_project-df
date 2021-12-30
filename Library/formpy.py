Sam's log scaling singularity fix
Easy global locals bug to fix singularities not plotting when Sam's logs are used.

Library/formpy.py
@@ -1480,7 +1480,7 @@ def form_2(xg, yg, form2, form_2_eq=None):
             '''
             
             
-            form2 = self.form_2  # from self, get 2-form too
+            form2 = self.form_2 * 1  # from self, get 2-form too
             
             # set all insignificant values to zero:
             form2[np.abs(form2) < 1e-12] = 0
@@ -1548,7 +1548,7 @@ def form_2(xg, yg, form2, form_2_eq=None):
             
             max_size = np.max(mag)   # careful with singularities, else ---> nan
 
-            if self.logarithmic_scale_bool == True:
+            if self.logarithmic_scale_bool:
                 # Add 1 to each magnitude
                 mag1 = mag + 1
                 # Calculate the appropriate scaling factor

Library/sam_examples.py
@@ -18,28 +18,28 @@ xg, yg = np.meshgrid(r, r)
 u = np.exp(xg*yg)
 v = np.sin(yg)
 
-fig1=plt.figure(figsize=(12,6))
+fig1 = plt.figure(figsize=(12, 6))
 ax1 = fig1.add_subplot(121)
 ax2 = fig1.add_subplot(122)
 
-f1 = fp.form_1(xg,yg,u,v)
+f1 = fp.form_1(xg, yg, u, v)
 f1.plot(ax1)
 
 f1.log_scaling()
 f1.plot(ax2)
 
-#%%
+# %%
 
 r = np.linspace(-5, 5, 21)
 xg, yg = np.meshgrid(r, r)
 
-z = np.exp(xg**2+yg**2)
+z = np.exp(xg**2 + yg**2)
 
-fig1=plt.figure(figsize=(12,6))
+fig1 = plt.figure(figsize=(12, 6))
 ax1 = fig1.add_subplot(121)
 ax2 = fig1.add_subplot(122)
 
-f2 = fp.form_2(xg,yg,z)
+f2 = fp.form_2(xg, yg, z)
 f2.plot(ax1)
 
 f2.log_scaling()