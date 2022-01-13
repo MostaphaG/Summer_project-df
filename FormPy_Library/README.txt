eb751bd96db8a0e6bff259bb89935da4efa6130f
Moustafa <moustafa.gharamti@nottingham.ac.uk>
2022-01-13 23:32:31 +0000

    preparing the library package

---
+++ FormPy_Library/README.txt
@@ -0,0 +1,4 @@
+# Summer_project-df
+## Python Library for differential forms
+
+This Python library deals with differential forms. It gives an interactive visualization of the differential forms and their calculus such as exterior derivative, interior derivative, wedge product and the Hodge star operation, and helps understand their geometric meaning. The library also plots contravariant vector fields and their different geometric structure such as gradient, divergence, curl and inner products between covariant and contravariant vectors.
---
+++ FormPy_Library/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='formpy',
+  version='1.0',
+  description='Differential forms and Exterior Algebra visualizer',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Moustafa Gharamti','Maciej Jarema', 'Samuel Kirwin-Jones'
+  author_email='moustafa.gharamti@nottingham.ac.uk','macusjarema@gmail.com', '1999samkj@gmail.com'
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='calculator', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
