eeea0791905ddd8a7d8755a2f586d952776500bc
Moustafa <moustafa.gharamti@nottingham.ac.uk>
2022-01-14 05:32:03 +0000

    setip.py edit
    Just resending this so it doesn't get lost. @MACIEK1JAREMA, @skirwinjones We are couple of setps from publishing the library. Just check the files in the FormPy_Library,  most of them are self explanatory. Main one is setup.py. I thought our development status is 5 - Production/Stable, check here https://martin-thoma.com/software-development-stages/ for details on this parameter, and let me know if you think we are in different status. For testing and update, please keep updating the original file in Library folder. All new changes will be added before we publish.

--- FormPy_Library/setup.py
+++ FormPy_Library/setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 classifiers = [
   'Development Status :: 5 - Production/Stable',
   'Intended Audience :: Education',
+  'Intended Audience :: Developors',
+  'Operating System :: MacOS :: MacOS X',
   'Operating System :: Microsoft :: Windows :: Windows 10',
   'License :: OSI Approved :: MIT License',
   'Programming Language :: Python :: 3'
@@ -14,11 +16,11 @@ setup(
   description='Differential forms and Exterior Algebra visualizer',
   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
   url='https://github.com/MostaphaG/Summer_project-df/tree/main/FormPy_Library',  
-  author='Moustafa Gharamti','Maciej Jarema', 'Samuel Kirwin-Jones'
-  author_email='moustafa.gharamti@nottingham.ac.uk','macusjarema@gmail.com', '1999samkj@gmail.com'
+  author='Moustafa Gharamti','Maciej Jarema', 'Samuel Kirwin-Jones',
+  author_email='moustafa.gharamti@nottingham.ac.uk','macusjarema@gmail.com', '1999samkj@gmail.com',
   license='MIT', 
   classifiers=classifiers,
-  keywords='calculator', 
+  keywords='Differential forms','Exterior algebra','Vector fields', 'Exterior derivative', 'Interior derivative', 'Hodge', 'Wedge', 'Curl', 'Gradiant', 'Divergence', 'Derivative',  
   packages=find_packages(),
   install_requires=[''] 
 )
